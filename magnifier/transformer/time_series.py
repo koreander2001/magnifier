662d437f500be9d0868e10a988769af42b3ca77e
koreander2001 <neokamiyama@gmail.com>
2022-02-01 19:30:28 +0900

    Bugfix StandardScaler3d.

--- magnifier/transformer/time_series.py
+++ magnifier/transformer/time_series.py
@@ -3,6 +3,7 @@ from typing import Callable, List, Optional
 
 import numpy as np
 from python_speech_features import mfcc
+from sklearn.exceptions import NotFittedError
 from sklearn.preprocessing import StandardScaler
 
 from ..base import BaseTransformer
@@ -39,19 +40,47 @@ class MFCC(BaseTransformer):
 
 @dataclass
 class StandardScaler3d(BaseTransformer):
-    scalers: List[StandardScaler] = field(default_factory=list)
+    _scalers: List[StandardScaler] = field(default_factory=list)
 
-    def fit(self, X, y=None) -> "StandardScaler3d":
-        self.scalers = list(
-            map(lambda i: StandardScaler().fit(X[:, i, :]), range(X.shape[1]))
+    def fit(self, X: np.ndarray, y: None = None) -> "StandardScaler3d":
+        self._check_X(X)
+
+        self._scalers = list(
+            map(
+                lambda i: StandardScaler().fit(X[:, :, i].reshape(-1, 1)),
+                range(X.shape[2]),
+            )
         )
 
         return self
 
     def transform(self, X: np.ndarray) -> np.ndarray:
+        self._check_X_on_transform(X)
+
         X_result = np.copy(X)
 
-        for i, scaler in enumerate(self.scalers):
-            X_result[:, i, :] = scaler.transform(X[:, i, :])
+        for i, scaler in enumerate(self._scalers):
+            X_result[:, :, i] = scaler.transform(X[:, :, i].reshape(-1, 1)).reshape(
+                X.shape[:-1]
+            )
 
         return X_result
+
+    def _check_X(self, X: np.ndarray) -> None:
+        if not isinstance(X, np.ndarray):
+            raise TypeError(f"Type of X must be np.ndarray, but given: {type(X)}")
+        if X.ndim != 3:
+            raise ValueError(
+                f"Number of dimensions of X must be 3, but given: {X.shape}."
+            )
+
+    def _check_X_on_transform(self, X: np.ndarray) -> None:
+        if not self._scalers:
+            raise NotFittedError(f"{type(self).__name__} is not fitted.")
+
+        self._check_X(X)
+
+        if X.shape[2] != len(self._scalers):
+            raise ValueError(
+                f"X.shape[1] must be equal to {len(self._scalers)}, but given: {X.shape}."
+            )
